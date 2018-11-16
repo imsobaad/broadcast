4ef765104f55503778bbd303d3115c492d418fa5
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-11-16 17:44:48 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+  console.log(`Broadcast Bot is Online`);
+
+});
+
+
+
+client.on('message', message => {
+if (message.content.split(' ')[0] == '#bc')
+ message.guild.members.forEach( member => {
+         if (!message.member.hasPermission("CONNECT"))  return;
+member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
+                                                            message.delete();
+});
+});
+
+
+client.login(process.env.BOT_TOKEN);
