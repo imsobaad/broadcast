0330d677316d38506319e3e65d479e07914f4ca0
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-11-20 19:19:07 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,15 +6,34 @@ client.on('ready', () => {
 });
 
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Quietness Server`,"http://twitch.tv/S-F")
+  console.log('Online..,!!')
+});
+
+
 
 client.on('message', message => {
 if (message.content.split(' ')[0] == '#bc')
  message.guild.members.forEach( member => {
-         if (!message.member.hasPermission("CONNECT"))  return;
+         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
 member.send( `${member} ` + "\n" + message.content.substr(3));
                                                             message.delete();
 });
 });
+
+
+
+client.on('message', message => {
+if (message.content.split(' ')[0] == '#Qbc')
+ message.guild.members.forEach( member => {
+         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+member.send(message.content.substr(3));
+                                                            message.delete();
+});
+});
+
 
 
 client.login(process.env.BOT_TOKEN);
